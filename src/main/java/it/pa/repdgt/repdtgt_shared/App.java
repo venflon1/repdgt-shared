100ec605347d4996e8dff32a31a0bd5d1c22bbba
venflon1 <roberto.amato.11.93@gmail.com>
2022-04-27 17:31:24 +0200

    init project

---
+++ src/main/java/it/pa/repdgt/repdtgt_shared/App.java
@@ -0,0 +1,13 @@
+package it.pa.repdgt.repdtgt_shared;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

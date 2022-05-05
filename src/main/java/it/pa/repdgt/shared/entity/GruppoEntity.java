68e76f79e0cb207a1d749d80afeb79902fd64ebd
venflon1 <roberto.amato.11.93@gmail.com>
2022-05-05 09:04:58 +0200

    Refactor: Aggiunto id auto_increment in GruppoEntity

--- src/main/java/it/pa/repdgt/shared/entity/GruppoEntity.java
+++ src/main/java/it/pa/repdgt/shared/entity/GruppoEntity.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -23,6 +25,7 @@ import lombok.Setter;
 public class GruppoEntity implements Serializable {
 	@Id
 	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 
 	@Column(name = "DESCRIZIONE")

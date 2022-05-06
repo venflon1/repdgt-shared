f937ace3c5408d003ddb806a167db8cc49855a6b
venflon1 <roberto.amato.11.93@gmail.com>
2022-05-06 11:18:48 +0200

    Refactor: Modificato nome da RuoliConstants a RuoliUtentiConstants

--- src/main/java/it/pa/repdgt/shared/constants/RuoliUtentiConstants.java
+++ src/main/java/it/pa/repdgt/shared/constants/RuoliUtentiConstants.java
@@ -1,15 +1,14 @@
 package it.pa.repdgt.shared.constants;
 
-public interface RuoliConstants {
-
-	public final String DTD = "DTD";
+public interface RuoliUtentiConstants {
+	public final String DTD  = "DTD";
 	public final String DSCU = "DSCU";
-	public final String REG = "REG";
-	public final String DEG = "DEG";
+	public final String REG  = "REG";
+	public final String DEG  = "DEG";
 	public final String REGP = "REGP";
 	public final String DEGP = "DEGP";
 	public final String REPP = "REPP";
 	public final String DEPP = "DEPP";
 	public final String FACILITATORE = "FAL";
-	public final String VOLONTARIO = "VOL";
+	public final String VOLONTARIO   = "VOL";
 }

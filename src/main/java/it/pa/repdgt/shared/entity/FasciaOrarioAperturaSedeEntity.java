7ed488d59a5f8d3ce8c9785ea915a5e9021d5e54
venflon1 <roberto.amato.11.93@gmail.com>
2022-05-20 15:03:01 +0200

    Feat: implementazione entity FasciaOrarioAperturaSedeEntity

---
+++ src/main/java/it/pa/repdgt/shared/entity/FasciaOrarioAperturaSedeEntity.java
@@ -0,0 +1,51 @@
+package it.pa.repdgt.shared.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "FASCIA_ORARIO_APERTURA_SEDE")
+@Setter
+@Getter
+public class FasciaOrarioAperturaSedeEntity implements Serializable {
+	private static final long serialVersionUID = -8849825300824271398L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
+	@Column(name = "GIORNO_APERTURA", nullable = false, unique = true)
+	private String giornoAperturaSede;
+	
+	@Column(name = "ORARIO_APERTURA")
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date orarioAperuturaSede;
+	
+	@Column(name = "ORARIO_CHIUSURA")
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date orarioChiusuraSede;
+
+	@Column(name = "SEDE_ID")
+	private Long idSede;
+	
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_CREAZIONE")
+	private Date dataOraCreazione;
+
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_AGGIORNAMENTO")
+	private Date dataOraAggiornamento;
+}

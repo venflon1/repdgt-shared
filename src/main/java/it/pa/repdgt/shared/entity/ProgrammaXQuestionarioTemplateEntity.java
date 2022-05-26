044d83929b678b09b2a6389fe46d7b0a728c6d21
venflon1 <roberto.amato.11.93@gmail.com>
2022-05-26 17:02:47 +0200

    Feat: aggiunti campi dataOraCreazione e dataOraAggiornamento

--- src/main/java/it/pa/repdgt/shared/entity/ProgrammaXQuestionarioTemplateEntity.java
+++ src/main/java/it/pa/repdgt/shared/entity/ProgrammaXQuestionarioTemplateEntity.java
@@ -1,10 +1,14 @@
 package it.pa.repdgt.shared.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import it.pa.repdgt.shared.entity.key.ProgrammaXQuestionarioTemplateKey;
 import lombok.Getter;
@@ -19,4 +23,12 @@ public class ProgrammaXQuestionarioTemplateEntity implements Serializable {
 
 	@EmbeddedId
 	private ProgrammaXQuestionarioTemplateKey programmaXQuestionarioTemplateKey;
+	
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_CREAZIONE")
+	private Date dataOraCreazione;
+
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_AGGIORNAMENTO")
+	private Date dataOraAggiornamento;
 }

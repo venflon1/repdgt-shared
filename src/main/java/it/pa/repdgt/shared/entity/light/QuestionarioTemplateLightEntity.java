181c8ccecce99ce62675e0dce633012aded46383
StefanoDiociaiuti <stefano.diociaiuti@accenture.com>
2022-05-26 11:17:48 +0200

    Refactor: cancellata variabile inutilizzata

--- src/main/java/it/pa/repdgt/shared/entity/light/QuestionarioTemplateLightEntity.java
+++ src/main/java/it/pa/repdgt/shared/entity/light/QuestionarioTemplateLightEntity.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -28,7 +26,4 @@ public class QuestionarioTemplateLightEntity implements Serializable {
 
 	@Column(name = "STATO", nullable = false)
 	private String stato;
-	
-	@Column(name = "TIPOLOGIA_QUESTIONARIO")
-	private String tipologiaQuestionario;
 }

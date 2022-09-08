bf95c24b65b2a6478377f729b57a11e4c8c8f202
roberto.amato <45068121+venflon1@users.noreply.github.com>
2022-09-08 11:43:20 +0200

    Feat: creata entity integrazioniUtente e messa in relazione OneToOne con Utente

---
+++ src/main/java/it/pa/repdgt/shared/entity/IntegrazioniUtenteEntity.java
@@ -0,0 +1,49 @@
+package it.pa.repdgt.shared.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "integrazioni_utente")
+@Setter
+@Getter
+public class IntegrazioniUtenteEntity implements Serializable { 
+	private static final long serialVersionUID = -2440129724556580907L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
+	@OneToOne
+ 	@JoinColumn(name = "UTENTE_ID", referencedColumnName = "ID")
+	@JsonIgnore
+	private UtenteEntity utente;
+
+	@Column(name = "UTENTE_REGISTRATO_WORKDOCS", nullable = false)
+	private Boolean utenteRegistratoInWorkdocs;
+	
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_CREAZIONE")
+	private Date dataOraCreazione;
+
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@Column(name = "DATA_ORA_AGGIORNAMENTO")
+	private Date dataOraAggiornamento;
+}
